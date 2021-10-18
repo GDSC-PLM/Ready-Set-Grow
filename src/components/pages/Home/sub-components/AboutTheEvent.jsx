ff1e00106ead012b49f706695adbbc21b88b4405
Ken Javier <kencarlo21@gmail.com>
2021-10-18 22:14:27 +0800

    Added about the event in home page

---
+++ src/components/pages/Home/sub-components/AboutTheEvent.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../styles/aboutTheEvent.css";
+
+const AboutTheEvent = () => {
+  return (
+    <div className="about-the-event">
+      <h2>About the Event</h2>
+      <p>
+        Ready Set Grow is a week-long series of career and profile-building
+        filled with webinars and workshops. The main goal is to gear up students
+        towards their journey in the professional world through the activities
+        prepared that would foster technology literacy, global competitiveness
+        and self-empowerment. Bounded by the purpose of creating and launching
+        the path to their career, we also aim to motivate them in attaining
+        their future employment in the sector and recognize their own value as
+        future workforce members.
+      </p>
+    </div>
+  );
+};
+
+export default AboutTheEvent;
