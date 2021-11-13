67679ab558e3beeb14ea7fe079d2d042376787be
marcusandrev <marcusandrev.mav@gmail.com>
2021-11-13 09:51:13 +0800

    responsivenes first draft

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -14,7 +14,6 @@ import { BsLinkedin } from "react-icons/bs";
 import { AiFillTwitterCircle } from "react-icons/ai";
 import { ReactComponent as DSCLogo } from "../../../assets/images2/Footer/svg/DSCLogo.svg";
 import BottomBars from "../../../assets/images2/Footer/png/Footer_GDSC_Branding_Colors.png";
-
 export default class Footer extends Component {
   render() {
     return (
@@ -24,58 +23,70 @@ export default class Footer extends Component {
         </div>
 
         <div className="socMedIcons">
-          <a
-            href="https://www.facebook.com/gdsc.plm"
-            target="_blank"
-            className="margin-spacing"
-          >
-            <FaFacebook className="socMedIconsIndiv" />
-          </a>
-          <a
-            href="https://www.linkedin.com/company/gdsc-plm/mycompany/"
-            target="_blank"
-            className="margin-spacing"
-          >
-            <BsLinkedin className="socMedIconsIndiv BsLinkedin" />
-          </a>
-          <a
-            href="https://gdsc.community.dev/pamantasan-ng-lungsod-ng-maynila/"
-            target="_blank"
-            className="margin-spacing"
-          >
-            <DSCLogo className="DSCLogo" />
-          </a>
-          <a
-            href="https://twitter.com/gdsc_plm"
-            target="_blank"
-            className="margin-spacing"
-          >
-            <AiFillTwitterCircle className="socMedIconsIndiv AiFillTwitterCircle" />
-          </a>
-          <a
-            href="https://www.instagram.com/gdsc_plm/"
-            target="_blank"
-            className="margin-spacing"
-          >
-            <FaInstagram className="socMedIconsIndiv" />
-          </a>
-          <a
-            href="https://www.youtube.com/channel/UC1Jd3Cb99nMdCp9_VHIWeag"
-            target="_blank"
-            className="margin-spacing"
-          >
-            <FaYoutube className="socMedIconsIndiv " />
-          </a>
-          <a
-            href="https://github.com/GDSC-PLM"
-            target="_blank"
-            className="margin-spacing"
-          >
-            <FaGithub className="socMedIconsIndiv " />
-          </a>
-          <a href="/" target="_blank" className="margin-spacing">
-            <FaDiscord className="socMedIconsIndiv " />
-          </a>
+          <div className="media-screen-up">
+            <a
+              href="https://www.facebook.com/gdsc.plm"
+              target="_blank"
+              className="margin-spacing"
+            >
+              <FaFacebook className="socMedIconsIndiv" />
+            </a>
+            <a
+              href="https://www.linkedin.com/company/gdsc-plm/mycompany/"
+              target="_blank"
+              className="margin-spacing"
+            >
+              <BsLinkedin className="socMedIconsIndiv BsLinkedin" />
+            </a>
+            <a
+              href="https://gdsc.community.dev/pamantasan-ng-lungsod-ng-maynila/"
+              target="_blank"
+              className="margin-spacing"
+            >
+              <DSCLogo className="DSCLogo" />
+            </a>
+            <a
+              href="https://twitter.com/gdsc_plm"
+              target="_blank"
+              className="margin-spacing"
+            >
+              <AiFillTwitterCircle className="socMedIconsIndiv AiFillTwitterCircle" />
+            </a>
+          </div>
+
+          <div className="media-screen-down">
+            <a
+              href="https://www.instagram.com/gdsc_plm/"
+              target="_blank"
+              className="margin-spacing ig-icon"
+            >
+              <FaInstagram className="socMedIconsIndiv" />
+            </a>
+            <a
+              href="https://www.youtube.com/channel/UC1Jd3Cb99nMdCp9_VHIWeag"
+              target="_blank"
+              className="margin-spacing yt-icon"
+            >
+              <FaYoutube className="socMedIconsIndiv " />
+            </a>
+
+           
+            <a
+              href="https://github.com/GDSC-PLM"
+              target="_blank"
+              className="margin-spacing github-icon "
+            >
+              <FaGithub className="socMedIconsIndiv  " />
+            </a>
+            <a
+              href="https://discord.gg/F6UjYff3Tm"
+              target="_blank"
+              className="margin-spacing discord-icon"
+            >
+              <FaDiscord className="socMedIconsIndiv" />
+            </a>
+            
+          </div>
         </div>
 
         <p>Copyright © 2021. GDSC- PLM. All rights Reserved</p>
